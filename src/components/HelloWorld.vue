86d06a1402d9b9c7491e27597ca99ac3e77fb5c7
FangRenShuo <109905666+FangRenShuo@users.noreply.github.com>
2022-10-18 19:14:40 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <el-button type="button" @click="count++">count is: {{ count }}</el-button>
+  
+</template>
+
+<script>
+import{ref} from "vue";
+import {ElButton} from "element-plus";
+export default  ({
+  components:{
+    ElButton,
+  },
+  setup() {
+    const count = ref(0);
+    return { 
+        count,
+    };
+  },
+})
+</script>
+<style scoped>
+  a{
+    color: #43b983;
+  }
+</style>
---
+++ src/views/Page2.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>我是P2的组件</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
